6e46af89af9ee59744597b42b5dd91386d3e54c8
luisegarduno <luis.e.garduno@gmail.com>
2023-06-19 01:55:47 -0400

    - Added initial feature tracking

---
+++ extractor.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+
+class Extractor(object):
+    # detection
+    def __init__(self):
+        # Initiate ORB detector
+        self.orb = cv2.ORB_create(100)
+        self.bf = cv2.BFMatcher()
+        self.last = None
+
+    # extraction
+    def extract(self, img):
+        feats = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8), 3000, qualityLevel=0.01, minDistance=3)
+
+        kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], size=20) for f in feats]
+        kps, des = self.orb.compute(img, kps)
+
+        # matching
+        matches = None
+        if self.last is not None:
+            matches = self.bf.match(des, self.last['des'])
+        self.last = {'kps': kps, 'des': des}
+
+        return kps, des, matches
--- slam.py
+++ slam.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
-import time
 import cv2
+import numpy as np
+from extractor import Extractor
 
 # Set Width & Height: 960x540
 W = 1920//2
 H = 1080//2
 
+fe = Extractor()
+
 def process_frame(img):
     # Resize video
     img = cv2.resize(img, (W, H))
-    cv2.imshow('image', img)
 
-    print("Resolution: ", img.shape)
+    # find the keypoints & compute the descriptors w/ ORB
+    kps, des, matches = fe.extract(img)
+
+    for p in kps:
+        u,v = map(lambda x: int(round(x)), p.pt)
+        cv2.circle(img, (u,v), color=(0,255,0), radius=3)
+
+    # Show window
+    cv2.imshow('TrySLAM', img)
 
 if __name__ == "__main__":
     # Load video
